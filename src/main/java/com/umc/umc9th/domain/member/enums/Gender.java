f5b91315011bf4d383edce99207a99ffb78a58f9
Synt4xT3rr0r(Function) <67085909+Function1790@users.noreply.github.com>
2025-10-05 18:22:24 +0900

    mission/#04 - Entity 코드 작성

---
+++ src/main/java/com/umc/umc9th/domain/member/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.plane.umc9th.domain.member.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/umc/umc9th/domain/member/enums/Provider.java
@@ -0,0 +1,8 @@
+package com.plane.umc9th.domain.member.enums;
+
+public enum Provider {
+    LOCAL,
+    GOOGLE,
+    KAKAO,
+    NAVER
+}
